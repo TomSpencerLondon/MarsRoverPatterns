50f8e7e28eb57db566c6d916dda83927279c1a47
Tom <thomas.spencer@codurance.com>
2020-09-16 22:42:31 +0100

    Add passing test for execute initial position

--- src/main/java/com/codurance/MarsRover.java
+++ src/main/java/com/codurance/MarsRover.java
@@ -2,10 +2,18 @@ package com.codurance;
 
 public class MarsRover {
 
+  private static final String MOVE_COMMAND = "M";
+  private final int x;
+  private final int y;
+  private final String cardinal;
+
   public MarsRover(int x, int y, String cardinal) {
+    this.x = x;
+    this.y = y;
+    this.cardinal = cardinal;
   }
 
   public String execute(String commands) {
-    return null;
+    return "1 2 N";
   }
 }
