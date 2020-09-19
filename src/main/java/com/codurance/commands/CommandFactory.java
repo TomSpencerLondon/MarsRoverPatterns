3119a4cd2e5a89ac4515847677fd690b6f01c4bc
Tom <thomas.spencer@codurance.com>
2020-09-19 08:38:05 +0100

    Add Passing tests!!!! Yay!!! commandFactory complete!!!!!

--- src/main/java/com/codurance/commands/CommandFactory.java
+++ src/main/java/com/codurance/commands/CommandFactory.java
@@ -1,24 +1,33 @@
 package com.codurance.commands;
 
 import com.codurance.Rover;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CommandFactory {
   private Rover rover;
+  private static final String MOVE = "M";
+  private static final String LEFT = "L";
+  private static final String RIGHT = "R";
+  private static final String EMPTY = "";
+  private Map<String, Command> commands;
 
   public CommandFactory(Rover rover) {
-    this.rover = rover;
+    initializeCommands(rover);
+  }
+
+  private void initializeCommands(Rover rover) {
+    commands = new HashMap<>(){
+      {
+        put(MOVE, new MoveCommand(rover));
+        put(LEFT, new MoveCommand(rover));
+        put(RIGHT, new MoveCommand(rover));
+        put(EMPTY, new MoveCommand(rover));
+      }
+    };
   }
 
   public Command commandFrom(String command){
-    if (command.equals("M")){
-      return new MoveCommand(rover);
-    }
-    if (command.equals("R")){
-      return new TurnRightCommand(rover);
-    }
-    if (command.equals("L")){
-      return new TurnLeftCommand(rover);
-    }
-    throw new UnsupportedOperationException("Situation not implemented");
+    return commands.get(command);
   }
 }
